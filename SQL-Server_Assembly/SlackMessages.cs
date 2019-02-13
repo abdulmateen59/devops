198c1940051fcac4f23ce04acf7709a519f5d537
dell <abdul.mateen59@yahoo.com>
2019-02-13 10:22:55 +0100

    .

---
+++ SQL-Server_Assembly/SlackMessages.cs
@@ -0,0 +1,77 @@
+﻿
+public class Message
+{
+    public string PostUrl { get; set; }
+    public string Text { get; set; }
+    public string Channel { get; set; }
+    public string Icon_Emoji { get; set; }
+    public string Username { get; set; }
+    private string PostData
+    {
+        get
+        {
+            StringBuilder postData = new StringBuilder();
+            postData.Append("payload={");
+
+            if (!String.IsNullOrEmpty(this.Text))
+            {
+                postData.Append("\"text\":\"" + this.Text + "\"");
+            }
+
+            if (!String.IsNullOrEmpty(this.Channel))
+            {
+                postData.Append(",\"channel\":\"" + this.Channel + "\"");
+            }
+
+            if (!String.IsNullOrEmpty(this.Icon_Emoji))
+            {
+                postData.Append(",\"icon_emoji\":\"" + this.Icon_Emoji + "\"");
+            }
+
+            if (!String.IsNullOrEmpty(this.Username))
+            {
+                postData.Append(",\"username\": \"" + this.Username + "\"");
+            }
+
+            postData.Append("}");
+            return postData.ToString();
+        }
+    }
+
+
+    public Message(string postUrl, string text, string channel = null, string icon_emoji = null, string username = null)
+    {
+        PostUrl = postUrl;
+        Text = text;
+        Channel = channel;
+        Icon_Emoji = icon_emoji;
+        Username = username;
+    }
+
+
+    public string Send()
+    {
+        return ProcessRequest(this.PostUrl, this.PostData);
+    }
+
+
+    private string ProcessRequest(string postUrl, string postData)
+    {
+        WebRequest request = WebRequest.Create(postUrl);
+        request.Method = "POST";
+        byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+        request.ContentType = "application/x-www-form-urlencoded";
+        request.ContentLength = byteArray.Length;
+        Stream dataStream = request.GetRequestStream();
+        dataStream.Write(byteArray, 0, byteArray.Length);
+        dataStream.Close();
+        WebResponse response = request.GetResponse();
+        dataStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(dataStream);
+        string responseFromServer = reader.ReadToEnd();
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+        return responseFromServer;
+    }
+}
---
+++ SQL-Server_Assembly/msgAssembly/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlClient;
+
+
+public partial class RealTimeLogs
+    {
+      /*  [Microsoft.SqlServer.Server.SqlProcedure]
+        public static void MySQLCLR()
+        {
+            SqlContext.Pipe.Send("This text is from the sample SQLCLR procedure MySQLCLR\n");
+        }*/
+    [SqlTrigger(Name = @"RealTimeLogs", Target = "[dbo].[t_report_result]", Event = "FOR INSERT")]
+    public static void RTLogs()
+    {
+        SqlCommand command=null;
+        SqlDataReader reader=null;
+        SqlTriggerContext triggContext = SqlContext.TriggerContext;
+      
+        switch (triggContext.TriggerAction)
+        {
+            case TriggerAction.Insert:
+                pushToSlack(command, reader, "Insert","AlgoExecLogs");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+/*
+ * Push Notifications to Slack
+ */
+    public static void pushToSlack(SqlCommand command,SqlDataReader reader,string action, string type)
+    {
+       
+        string postdata = null;
+        using (SqlConnection connection = new SqlConnection(@"context connection=true"))
+        {
+
+            connection.Open();
+            command = new SqlCommand(@"SELECT * FROM INSERTED;", connection);   
+            reader = command.ExecuteReader();
+            reader.Read();
+
+
+            postdata = "*Realtime Algorithm Reporting* \n" + " Algorithm ID : "
+            + (string)reader[1].ToString() +
+            "\n Test Case Status : "
+            + (string)reader[4].ToString() +
+             "\n Test Case Details : "
+            + (string)reader[5].ToString()
+            + "\n Execution Time : "
+            + (string)reader[6].ToString();
+
+
+            string webhookUrl = "https://hooks.slack.com/services/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";
+            Message message = new Message(webhookUrl, postdata, "#realtime_logs", ":beetle:", "QA-LOG", true);
+            message.Send();
+            reader.Close();
+
+        }
+    }
+}
